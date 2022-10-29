1c868d5cfc98ac4a2a30d6723cb623eec0ac6a43
Ostap <electromechanicpost@gmail.com>
2022-10-29 16:44:34 +0300

    Added sounds

---
+++ Pulyuy/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+
+    [SerializeField] private AudioSource music;
+    [SerializeField] private AudioSource shoot;
+    [SerializeField] private AudioSource generatorWorking;
+    [SerializeField] private AudioSource generatorOverheat;
+    [SerializeField] private AudioSource xrayOn;
+    [SerializeField] private AudioSource xrayOff;
+    [SerializeField] private AudioSource doorOpen;
+    [SerializeField] private AudioSource doorClose;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Instance freak up");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        music.Play();
+    }
+
+    public static void PlayOverHeat() { instance.generatorOverheat.Play(); }
+
+    public static void PlayShoot() { instance.shoot.Play(); }
+
+    public static void SetGeneratorPlaying(bool value)
+    {
+        if (value) instance.generatorWorking.Play();
+        else instance.generatorWorking.Stop();
+    }
+
+    public static void SetXray(bool value)
+    {
+        if (value) instance.xrayOn.Play();
+        else instance.xrayOff.Play();
+    }
+
+    public static void SetDor(bool value)
+    {
+        if (value) instance.doorOpen.Play();
+        else instance.doorClose.Stop();
+    }
+}
